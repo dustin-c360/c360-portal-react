e99402dc8751b429f96233830240042c20d8e89e
Dustin Chung <dustin.chung@itsg003342-mac.lan>
2021-01-04 23:17:16 +0800

    Comment out "value" to prevent error due to unuse

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Dashboard from './common/Dashboard.js'
 
 export class App extends React.Component  {
   render()  {
-    const value = 'World!!';
+    //const value = 'World!!';
     //return <div>Hello {value}</div>;
     return <Dashboard />
   }
